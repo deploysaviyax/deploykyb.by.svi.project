bb389c0478fc43d8c3bfb472e1b641404643c088
deploysaviyax <deploysaviya@hotmail.com>
2024-10-26 18:42:19 +0530

    Update yasiya-api.js

--- plugins/yasiya-api.js
+++ plugins/yasiya-api.js
@@ -1,1 +1,7 @@
-
+const config = require('../config')
+var os = require('os')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const mg = require('../lib/mg')
+const axios = require('axios');
+const cine = require('../lib/cine');
